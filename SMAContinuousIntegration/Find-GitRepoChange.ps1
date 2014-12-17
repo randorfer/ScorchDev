566922f4ffbae9e0d092ec71e3bc8445698848e2
randorfer <ryan.andorfer@gmail.com>
2014-12-16 19:20:36 -0600

    add function to find git repo changes

---
+++ SMAContinuousIntegration/Find-GitRepoChange.ps1
@@ -0,0 +1,40 @@
+﻿<#
+    .Synopsis
+        Check the target Git Repo / Branch for any updated files
+#>
+Workflow Find-GitRepoChange
+{
+    Param([Parameter(Mandatory=$true) ] $Path,
+          [Parameter(Mandatory=$true) ] $Branch,
+          [Parameter(Mandatory=$false)] $WebserviceEndpoint = 'https://localhost')
+    
+    Write-Verbose -Message "Starting [$WorkflowCommandName]"
+
+    $ErrorActionPreference = 'Stop'
+
+    inlinescript
+    {
+        $Path   = $Using:Path
+        $Branch = $Using:Branch
+
+        # Set Location to the target repo and initialize
+        Set-Location $Path
+        $ErrorActionPreference = 'Continue'
+        $initialization = (git init) 2> $null
+        $ErrorActionPreference = 'Stop'
+
+        if(-not ((git branch) -contains "* $Branch"))
+        {
+            Write-Verbose -Message "Setting current branch to [$Branch]"
+            $ErrorActionPreference = 'Continue'
+            $output = (git checkout $Branch) 2> $null
+            $ErrorActionPreference = 'Stop'
+        }
+        else
+        {
+            Write-Verbose -Message "Branch already set to [$Branch]"
+        }
+    }
+
+    Write-Verbose -Message "Finished [$WorkflowCommandName]"
+}
