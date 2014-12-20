c76757eefcb12532cffa1f0693f3f10991aa8ed7
Ryan Andorfer <Ryan.Andorfer@gmail.com>
2014-12-20 10:42:14 -0600

    dummy version of git ps module for automation

---
+++ PowerShellModule/SCOrchDev-Git/SCOrchDev-Git.psm1
@@ -0,0 +1,80 @@
+﻿<#
+.SYNOPSIS
+    Converts an input string into a boolean value.
+
+.DESCRIPTION
+    $values = @($null, [String]::Empty, "True", "False", 
+                "true", "false", "    true    ", "0", 
+                "1", "-1", "-2", '2', "string", 'y', 'n'
+                'yes', 'no', 't', 'f');
+    foreach ($value in $values) 
+    {
+        Write-Verbose -Message "[$($Value)] Evaluated as [`$$(ConvertTo-Boolean -InputString $value)]" -Verbose
+    }                                     
+
+    VERBOSE: [] Evaluated as [$False]
+    VERBOSE: [] Evaluated as [$False]
+    VERBOSE: [True] Evaluated as [$True]
+    VERBOSE: [False] Evaluated as [$False]
+    VERBOSE: [true] Evaluated as [$True]
+    VERBOSE: [false] Evaluated as [$False]
+    VERBOSE: [    true    ] Evaluated as [$True]
+    VERBOSE: [0] Evaluated as [$False]
+    VERBOSE: [1] Evaluated as [$True]
+    VERBOSE: [-1] Evaluated as [$True]
+    VERBOSE: [-2] Evaluated as [$True]
+    VERBOSE: [2] Evaluated as [$True]
+    VERBOSE: [string] Evaluated as [$True]
+    VERBOSE: [y] Evaluated as [$True]
+    VERBOSE: [n] Evaluated as [$False]
+    VERBOSE: [yes] Evaluated as [$True]
+    VERBOSE: [no] Evaluated as [$False]
+    VERBOSE: [t] Evaluated as [$True]
+    VERBOSE: [f] Evaluated as [$False]
+
+.PARAMETER InputString
+    The string value to convert
+#>
+Function ConvertTo-Boolean
+{
+    Param($InputString)
+
+    if(-not [System.String]::IsNullOrEmpty($InputString))
+    {
+        $res    = $true
+        $success = [bool]::TryParse($InputString,[ref]$res)
+        if($success)
+        { 
+            return $res
+        }
+        else
+        {
+            $InputString = ([string]$InputString).ToLower()
+    
+            Switch ($InputString)
+            {
+                'f'     { $false }
+                'false' { $false }
+                'off'   { $false }
+                'no'    { $false }
+                'n'     { $false }
+                default
+                {
+                    try
+                    {
+                        return [bool]([int]$InputString)
+                    }
+                    catch
+                    {
+                        return [bool]$InputString
+                    }
+                }
+            }
+        }
+    }
+    else
+    {
+        return $false
+    }
+}
+Export-ModuleMember -Function *
