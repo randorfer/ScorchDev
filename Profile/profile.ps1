4a646254722fc0c2a64a82d3f84014beb823eb71
Ryan Andorfer <Ryan.Andorfer@gmail.com>
2015-02-09 16:01:03 -0600

    cleanup example profile

--- Profile/profile.ps1
+++ Profile/profile.ps1
@@ -1,17 +1,6 @@
 # SMA setup
 
 ## Modules
-<#
-$env:AOWorkspace = 'C:\TFS\Orchestrator'
-$env:SMAModulePath = "$env:AOWorkspace\PowerShellModulesV2"
-$env:SMARunbookPath = "$env:AOWorkspace\SMA"
-$ModulePaths = (Get-ChildItem $env:SMAModulePath | ForEach { $_.FullName + '\Dev' }) -Join ';'
-$env:PSModulePath = "$ModulePaths;$env:AOWorkspace\PowerShellScripts;$env:PSModulePath"
-$WebServiceEndpoint = 'https://localhost'
-$WebInfrastructureSMARunbookPath = "$env:AOWorkspace\..\WebInfrastructure\SMA"
-$env:LocalSMAVariableUpdateInterval = 0
-$AIDCred = $null
-#>
 $env:AutomationWorkspace = 'C:\GIT\ScorchDev'
 $env:SMAModulePath = "$env:AutomationWorkspace\PowerShellModules"
 $env:SMARunbookPath = "$env:AutomationWorkspace\SMA"
