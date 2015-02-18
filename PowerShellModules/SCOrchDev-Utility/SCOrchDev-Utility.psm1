9fd8aab2bb81675fbaea889f32aa11b599bbc2e2
Ryan Andorfer <Ryan.Andorfer@gmail.com>
2015-02-17 19:40:59 -0600

    do the conversion to utf8 for the module files

--- PowerShellModules/SCOrchDev-Utility/SCOrchDev-Utility.psm1
+++ PowerShellModules/SCOrchDev-Utility/SCOrchDev-Utility.psm1
@@ -360,4 +360,78 @@ Function New-TempDirectory
 
     New-Item -ItemType Directory $TempDirectory
 }
+
+<#
+    .SYNOPSIS
+        Gets file encoding. From http://poshcode.org/2059
+    
+    .DESCRIPTION
+        The Get-FileEncoding function determines encoding by looking at Byte Order Mark (BOM).
+        Based on port of C# code from http://www.west-wind.com/Weblog/posts/197245.aspx
+    
+    .EXAMPLE
+        Get-ChildItem  *.ps1 | select FullName, @{n='Encoding';e={Get-FileEncoding $_.FullName}} | where {$_.Encoding -ne 'ASCII'}
+        This command gets ps1 files in current directory where encoding is not ASCII
+
+    .EXAMPLE
+        Get-ChildItem  *.ps1 | select FullName, @{n='Encoding';e={Get-FileEncoding $_.FullName}} | where {$_.Encoding -ne 'ASCII'} | foreach {(get-content $_.FullName) | set-content $_.FullName -Encoding ASCII}
+        Same as previous example but fixes encoding using set-content
+#>
+
+
+Function Get-FileEncoding
+{
+    Param ( 
+            [Parameter(Mandatory = $True, ValueFromPipelineByPropertyName = $True)] 
+            [string]$Path 
+          )
+ 
+    [byte[]]$byte = get-content -Encoding byte -ReadCount 4 -TotalCount 4 -Path $Path
+    if ( $byte[0] -eq 0xef -and $byte[1] -eq 0xbb -and $byte[2] -eq 0xbf )
+    { 
+        Write-Output 'UTF8' 
+    }
+    elseif ($byte[0] -eq 0xfe -and $byte[1] -eq 0xff)
+    { 
+        Write-Output 'Unicode' 
+    }
+    elseif ($byte[0] -eq 0 -and $byte[1] -eq 0 -and $byte[2] -eq 0xfe -and $byte[3] -eq 0xff)
+    { 
+        Write-Output 'UTF32' 
+    }
+    elseif ($byte[0] -eq 0x2b -and $byte[1] -eq 0x2f -and $byte[2] -eq 0x76)
+    { 
+        Write-Output 'UTF7'
+    }
+    else
+    { 
+        Write-Output 'ASCII' 
+    }
+}
+<#
+    .Synopsis
+#>
+Function ConvertTo-UTF8
+{
+    Param ( 
+            [Parameter(Mandatory = $True, ValueFromPipelineByPropertyName = $True)] 
+            [string]$Path 
+          )
+    $File = Get-Item $Path
+    $content = Get-Content $Path
+    if ( $content -ne $null ) 
+    {
+        Remove-Item -Path $file.FullName -Force
+        $content | Out-File -FilePath $file.FullName -Encoding utf8
+    } 
+    else
+    {
+        Throw-Exception -Type 'NoContentFound' `
+                        -Message 'Could not read the file' `
+                        -Property @{ 'Path' = $Path ;
+                                     'File' = $(ConvertTo-JSON $File) ;
+                                     'Content' = $content }
+    }
+}
+
 Export-ModuleMember -Function * -Verbose:$false -Debug:$false
