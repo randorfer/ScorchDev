eb168ab1fdd7f462579ed2c8787ead20fd5e852e
Ryan Andorfer <Ryan.Andorfer@gmail.com>
2015-02-21 13:14:46 -0600

    Add in SQL module

---
+++ PowerShellModules/SCOrchDev-SQL/SCOrchDev-SQL.psm1
@@ -0,0 +1,75 @@
+﻿<# 
+ .Synopsis
+  Uses ADO .NET to query SQL
+
+ .Description
+  Queries a SQL Database and returns a datatable of results
+
+ .Parameter query
+  The SQL Query to run
+ 
+ .Parameter parameters
+  A list of SQLParameters to pass to the query
+
+  .Parameter connectionString
+   Sql Connection string for the DB to connect to
+
+   .Parameter timeout
+   timeout property for SQL query. Default is 60 seconds
+
+ .Example
+   # run a simple query
+
+   $connectionString = ""
+   $parameters = @{}
+   Invoke-SqlQuery -query "SELECT GroupID, GroupName From [dbo].[Group] WHERE GroupName=@GroupName" -parameters @{"@GroupName"="genmills\groupName"} -connectionString $connectionString;
+   Invoke-SqlQuery -query "SELECT GroupID, GroupName From [dbo].[Group]" -parameters @{} -connectionString $connectionString;
+   
+#>
+function Invoke-SqlQuery([string]$query, [System.Collections.Hashtable] $parameters, [string] $connectionString, [int]$timeout=60)
+{
+    # convert parameter string to array of SqlParameters
+    try
+    {
+		Write-Verbose -Message "`$query [$query]"
+		Write-Verbose -Message "`$connectionString [$connectionString]"
+		Write-Verbose -Message "`$timeout [$timeout]"
+		foreach($paramKey in $parameters.Keys)
+		{
+			Write-Verbose -Message "`$paramKey   [$paramKey]"
+			Write-Verbose -Message "`$ParamValue [$($parameters[$paramkey])]"
+		}
+        $sqlConnection = new-object System.Data.SqlClient.SqlConnection $connectionString
+        $sqlConnection.Open()
+
+        #Create a command object
+        $sqlCommand = $sqlConnection.CreateCommand()
+        $sqlCommand.CommandText = $query;
+        if($parameters)
+        {
+            foreach($key in $parameters.Keys)
+            {
+                $sqlCommand.Parameters.AddWithValue($key, $parameters[$key]) | Out-Null
+            }
+        }
+		
+		$sqlCommand.CommandTimeout = $timeout
+
+        #Execute the Command
+        $sqlReader = $sqlCommand.ExecuteReader()
+
+        $Datatable = New-Object System.Data.DataTable
+        $DataTable.Load($SqlReader)
+
+
+        return $DataTable;
+    }
+    finally
+    {
+        if($sqlConnection -and $sqlConnection.State -ne [System.Data.ConnectionState]::Closed)
+        {
+            $sqlConnection.Close();
+        }
+    }
+}
+Export-ModuleMember -Function * -Verbose:$false
