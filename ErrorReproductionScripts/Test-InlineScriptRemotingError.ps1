fbccdf563ee527bb72acf44de170617740e75f1d
Ryan Andorfer <Ryan.Andorfer@gmail.com>
2015-03-02 11:10:58 -0600

    add note that error is fixed in PSv5

--- ErrorReproductionScripts/Test-InlineScriptRemotingError.ps1
+++ ErrorReproductionScripts/Test-InlineScriptRemotingError.ps1
@@ -1,6 +1,8 @@
 ﻿<#
     Here’s an example of a simpler workflow that demonstrates the bug:
     It generates an error after the third iteration. But because of the ErrorActionPreference, the $i variable isn’t updated appropriately and you get 10 iterations.
+
+    Fixed in PSv5
 #>
 
 workflow Test-InlineScriptRemotingError
@@ -23,7 +25,7 @@ workflow Test-InlineScriptRemotingError
 
 workflow Test-InlineScriptRemotingErrorWorkaround
 {
-        foreach ($i in 1..10)
+    foreach ($i in 1..10)
     {
         inlinescript
         {
@@ -39,5 +41,4 @@ workflow Test-InlineScriptRemotingErrorWorkaround
             }
         } -PSComputerName localhost
     }
-
 }
