4e2bd7e871a91adf4ad3c10253f69a445046a8b1
Ryan Andorfer <Ryan.Andorfer@gmail.com>
2015-03-06 10:12:54 -0600

    add in function for removing orphan modules

---
+++ Runbooks/SMAContinuousIntegration/Remove-SmaOrphanModule.ps1
@@ -0,0 +1,46 @@
+﻿<#
+    .Synopsis
+        Checks a SMA environment and removes any modules that are not found
+        in the local psmodulepath
+#>
+Workflow Remove-SmaOrphanModule
+{
+    Write-Verbose -Message "Starting [$WorkflowCommandName]"
+    $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+
+    $CIVariables = Get-BatchAutomationVariable -Name @('SMACredName',
+                                                       'WebserviceEndpoint'
+                                                       'WebservicePort') `
+                                               -Prefix 'SMAContinuousIntegration'
+    $SMACred = Get-AutomationPSCredential -Name $CIVariables.SMACredName
+
+    $SmaModule = Get-SmaModule -WebServiceEndpoint $CIVariables.WebserviceEndpoint `
+                               -Port $CIVariables.WebservicePort `
+                               -Credential $SMACred
+
+    $LocalModule = Get-Module -ListAvailable -Refresh -Verbose:$false
+
+    if(-not ($SmaModule -and $LocalModule))
+    {
+        if(-not $SmaModule)   { Write-Warning -Message 'No modules found in SMA. Not cleaning orphan modules' }
+        if(-not $LocalModule) { Write-Warning -Message 'No modules found in local PSModule Path. Not cleaning orphan modules' }
+    }
+    else
+    {
+        $ModuleDifference = Compare-Object -ReferenceObject  $SmaModule.ModuleName `
+                                           -DifferenceObject $LocalModule.Name
+        Foreach($Difference in $ModuleDifference)
+        {
+            if($Difference.SideIndicator -eq '<=')
+            {
+                Write-Verbose -Message "[$($Difference.InputObject)] Does not exist in Source Control"
+                Remove-SmaModule -Name $Difference.InputObject `
+                                 -WebServiceEndpoint $CIVariables.WebserviceEndpoint `
+                                 -Port $CIVariables.WebservicePort `
+                                 -Credential $SMACred
+                Write-Verbose -Message "[$($Difference.InputObject)] Removed from SMA"
+            }
+        }
+    }
+    Write-Verbose -Message "Finished [$WorkflowCommandName]"
+}
