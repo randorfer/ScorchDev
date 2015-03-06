98ad12042c6e053cd61445f4295e985d484a51a7
Ryan Andorfer <Ryan.Andorfer@gmail.com>
2015-03-06 15:29:53 -0600

    uncomment runbook publishing

--- Installer/Deploy-Integration.ps1
+++ Installer/Deploy-Integration.ps1
@@ -7,11 +7,11 @@ workflow Deploy-Integration
     
     Write-Verbose -Message "Starting [$WorkflowCommandName]"
     $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
-    <#
+
     Foreach($RunbookFile in (Get-ChildItem -Path ..\Runbooks -Recurse -Filter *.ps1))
     {
         Publish-SMARunbookChange -FilePath $RunbookFile.FullName -CurrentCommit $currentcommit -RepositoryName $repositoryname
-    }#>
+    }
     Foreach($SettingsFile in (Get-ChildItem -Path ..\Runbooks -Recurse -Filter *.json))
     {
         Publish-SMASettingsFileChange -FilePath $SettingsFile.FullName -CurrentCommit $currentcommit -RepositoryName $repositoryname
