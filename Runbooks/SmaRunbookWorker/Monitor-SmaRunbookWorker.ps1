ad2f48318b1ca02fb9ce5e0db77e98fe961f7da8
Ryan Andorfer <Ryan.Andorfer@gmail.com>
2015-03-07 09:06:52 -0600

    empty shell of what is yet to come

---
+++ Runbooks/SmaRunbookWorker/Monitor-SmaRunbookWorker.ps1
@@ -0,0 +1,10 @@
+<#
+    .Synopsis
+        Monitors Sma Runbook workers. When they are found to be unhealthy
+        invokes a process dump for the top processes running
+#>
+
+Workflow Monitor-SmaRunbookWorker
+{
+    
+}
