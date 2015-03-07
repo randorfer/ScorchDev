89c9d8137d8b9ddf99acaa37fb3d1bd1a3e403be
Ryan Andorfer <Ryan.Andorfer@gmail.com>
2015-03-07 12:40:35 -0600

    start moving to a monitor sytle for smarunbook worker memory error handling

--- Runbooks/SmaRunbookWorker/Monitor-SmaRunbookWorker.ps1
+++ Runbooks/SmaRunbookWorker/Monitor-SmaRunbookWorker.ps1
@@ -6,5 +6,48 @@
 
 Workflow Monitor-SmaRunbookWorker
 {
-    
+    Write-Verbose -Message "Starting [$WorkflowCommandName]"
+    $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+
+    $SmaRunbookWorkerVars = Get-BatchAutomationVariable -Name @('WebServiceEndpoint',
+                                                                'WebServicePort',
+                                                                'AccessCredName',
+                                                                'MinimumPercentFreeMemory',
+                                                                'MinimumProcDumpPerDay',
+                                                                'DaysToKeepProcDump',
+                                                                'ProcDumpPath',
+                                                                'ProcessesToDumpJSON') `
+                                                        -Prefix 'SmaRunbookWorker'
+    $AccessCred = Get-AutomationPSCredential -Name $SmaRunbookWorkerVars.AccessCredName
+    $Worker = Get-SMARunbookWorker
+
+    Do
+    {
+        Foreach -Parallel ($Worker in (Get-SmaRunbookWorker))
+        {
+            $DumpPath = "$($SmaRunbookWorkerVars.ProcDumpPath)\$(Get-Date -Format MM-d-yyyy)\$($env:COMPUTERNAME)"
+            $WorkerStatus = Test-SmaRunbookWorker -RunbookWorker $Worker `
+                                                  -MinimumPercentFreeMemory $SmaRunbookWorkerVars.MinimumPercentFreeMemory `
+                                                  -AccessCred $AccessCred
+        
+            if($WorkerStatus -eq 'Healthy')
+            {
+                # Check if there has been a process dump in the last 24 hours. If not create process dump
+            }
+            else
+            {
+                Invoke-RemoteProcDump -ComputerName $Worker `
+                                      -DumpPath $DumpPath `
+                                      -ProcessList $SmaRunbookWorkerVars.ProcessesToDumpJSON `
+                                      -AccessCredName $SmaRunbookWorkerVars.AccessCredName
+            }
+
+            # Cleanup old process dumps
+            # Implement Delay Cycle
+        }
+    }
+    While($MonitorActive)
+
+    Start-SmaRunbook -Name $WorkflowCommandName
+    Write-Verbose -Message "Finished [$WorkflowCommandName]"
 }
---
+++ Runbooks/SmaRunbookWorker/Test-SmaRunbookWorker.ps1
@@ -0,0 +1,56 @@
+<#
+    .Synopsis
+        Tests a runbook worker to see if it is above or below the target memory
+        threshold
+
+    .Parameter RunbookWorker
+        The runbook worker to test
+
+    .Parameter MinimumPercentFreeMemory
+        The minimum % Free memory before going to a 'unhealthy' state
+
+    .Parameter AccessCred
+        The pscredential to use when connecting to the runbook worker
+
+    .Return
+        Returns either 'Healthy' or 'Unhealthy'
+#>
+
+Workflow Test-SmaRunbookWorker
+{
+    Param([Parameter(Mandatory=$True) ][string]       $RunbookWorker,
+          [Parameter(Mandatory=$False)][int]          $MinimumPercentFreeMemory = 5,
+          [Parameter(Mandatory=$True) ][pscredential] $AccessCred)
+
+    InlineScript
+    {
+        $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Continue
+        & {
+            $null = $(
+                $DebugPreference       = [System.Management.Automation.ActionPreference]$Using:DebugPreference
+                $VerbosePreference     = [System.Management.Automation.ActionPreference]$Using:VerbosePreference
+                $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+
+                $MinimumPercentFreeMemory = $Using:MinimumPercentFreeMemory
+
+                $Win32OperatingSystem = Get-WmiObject -Class win32_OperatingSystem
+                $CurrentPercentFreeMemory = [int](($Win32OperatingSystem.FreePhysicalMemory / $Win32OperatingSystem.TotalVisibleMemorySize) * 100)
+
+                Write-Verbose "[$($Env:ComputerName)] % Free Memory [$($CurrentPercentFreeMemory)%]"
+                if($CurrentPercentFreeMemory -le $MinimumPercentFreeMemory)
+                {
+                    Write-Warning -Message "[$($Env:ComputerName)] % Free Memory [$($CurrentPercentFreeMemory)%]"
+                    Write-Warning -Message "[$($Env:ComputerName)] is below free memory threshold of [$($MinimumPercentFreeMemory)%]"
+                    $ReturnStatus = 'Unhealthy'
+                }
+                else
+                {
+                    Write-Verbose "[$($Env:ComputerName)] % Free Memory [$($CurrentPercentFreeMemory)%]"
+                    Write-Verbose -Message "[$($Env:ComputerName)] is above free memory threshold of [$($MinimumPercentFreeMemory)%]"
+                    $ReturnStatus = 'Healthy'
+                }
+            )
+            Return $ReturnStatus
+        }
+    } -PSComputerName $RunbookWorker -PSCredential $AccessCred
+}
