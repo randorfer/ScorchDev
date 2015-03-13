708bcd81d24554221958ebdedb0b8c7455ad0362
Ryan Andorfer <Ryan.Andorfer@gmail.com>
2015-03-13 15:53:32 -0500

    sign and reformat

--- ErrorReproductionScripts/Test-ReservedWordAssignment.ps1
+++ ErrorReproductionScripts/Test-ReservedWordAssignment.ps1
@@ -1,9 +1,4 @@
-﻿workflow Test-ReservedWordAssignment
-{
-    $To = 'somebody@somewhere.net'
-}
-
-<#
+﻿<#
 Produces this:
 
 The workflow 'Test-ReservedWordAssignment' could not be started: The following errors were encountered while processing the workflow tree:
@@ -15,3 +10,31 @@ At line:383 char:21
     + CategoryInfo          : InvalidArgument: (System.Manageme...etersDictionary:PSBoundParametersDictionary) [], RuntimeException
     + FullyQualifiedErrorId : StartWorkflow.InvalidArgument
 #>
+workflow Test-ReservedWordAssignment
+{
+    $To = 'somebody@somewhere.net'
+}
+
+# SIG # Begin signature block
+# MIID1QYJKoZIhvcNAQcCoIIDxjCCA8ICAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUaNHB5dxPd6V6us/6wPWZC80b
+# 9rOgggH3MIIB8zCCAVygAwIBAgIQEdV66iePd65C1wmJ28XdGTANBgkqhkiG9w0B
+# AQUFADAUMRIwEAYDVQQDDAlTQ09yY2hEZXYwHhcNMTUwMzA5MTQxOTIxWhcNMTkw
+# MzA5MDAwMDAwWjAUMRIwEAYDVQQDDAlTQ09yY2hEZXYwgZ8wDQYJKoZIhvcNAQEB
+# BQADgY0AMIGJAoGBANbZ1OGvnyPKFcCw7nDfRgAxgMXt4YPxpX/3rNVR9++v9rAi
+# pY8Btj4pW9uavnDgHdBckD6HBmFCLA90TefpKYWarmlwHHMZsNKiCqiNvazhBm6T
+# XyB9oyPVXLDSdid4Bcp9Z6fZIjqyHpDV2vas11hMdURzyMJZj+ibqBWc3dAZAgMB
+# AAGjRjBEMBMGA1UdJQQMMAoGCCsGAQUFBwMDMB0GA1UdDgQWBBQ75WLz6WgzJ8GD
+# ty2pMj8+MRAFTTAOBgNVHQ8BAf8EBAMCB4AwDQYJKoZIhvcNAQEFBQADgYEAoK7K
+# SmNLQ++VkzdvS8Vp5JcpUi0GsfEX2AGWZ/NTxnMpyYmwEkzxAveH1jVHgk7zqglS
+# OfwX2eiu0gvxz3mz9Vh55XuVJbODMfxYXuwjMjBV89jL0vE/YgbRAcU05HaWQu2z
+# nkvaq1yD5SJIRBooP7KkC/zCfCWRTnXKWVTw7hwxggFIMIIBRAIBATAoMBQxEjAQ
+# BgNVBAMMCVNDT3JjaERldgIQEdV66iePd65C1wmJ28XdGTAJBgUrDgMCGgUAoHgw
+# GAYKKwYBBAGCNwIBDDEKMAigAoAAoQKAADAZBgkqhkiG9w0BCQMxDAYKKwYBBAGC
+# NwIBBDAcBgorBgEEAYI3AgELMQ4wDAYKKwYBBAGCNwIBFTAjBgkqhkiG9w0BCQQx
+# FgQUioXoXxdjBvgTt/2IEc0UyPy/oKwwDQYJKoZIhvcNAQEBBQAEgYAmQfADRB29
+# Ts7KK7G/tQnaRO3leq9A9rCFNN6lR5yFPWezIlk7M1foD3Pot/NKG8X083UrNS28
+# ywyJ9QJ1kP0lPCZfKMA8rjn1UWDgPrNfDTki1uGtSGeUqb2amngAbVvWes6+qTvv
+# n5Gzn1uGHomq9XKUKXh7SdIp5xr+7MaN4Q==
+# SIG # End signature block
--- LocalPowerShellModules/LocalDev/EmulatedAutomationActivitiesInner.psm1
+++ LocalPowerShellModules/LocalDev/EmulatedAutomationActivitiesInner.psm1
@@ -1,67 +1,75 @@
 ﻿$SmaWebServiceDetails = @{
-    "WebServiceEndpoint" = "https://scorchsma01.scorchdev.com";
-    "Port" = 9090;
-    "AuthenticationType" = "Windows";
+    'WebServiceEndpoint' = 'https://scorchsma01.scorchdev.com'
+    'Port'             = 9090
+    'AuthenticationType' = 'Windows'
 }
 
 
-    # Uncomment this section and fill in $CredUsername and $CredPassword values
-    # to talk to SMA using Basic Auth instead of Windows Auth
+# Uncomment this section and fill in $CredUsername and $CredPassword values
+# to talk to SMA using Basic Auth instead of Windows Auth
 
-    # username / password of an account with access to the SMA Web Service
-    $CredUsername = "scorchdev\sma"
-    $CredPassword = "TechEd_2014"
+# username / password of an account with access to the SMA Web Service
+$CredUsername = 'scorchdev\sma'
+$CredPassword = 'TechEd_2014'
     
-    $SecurePassword = $CredPassword | ConvertTo-SecureString -asPlainText -Force
+$SecurePassword = $CredPassword | ConvertTo-SecureString -AsPlainText -Force
    
-    $SmaWebServiceDetails.AuthenticationType = "Basic"
-    $SmaWebServiceDetails.Credential = New-Object System.Management.Automation.PSCredential($CredUsername,$SecurePassword)
+$SmaWebServiceDetails.AuthenticationType = 'Basic'
+$SmaWebServiceDetails.Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ($CredUsername, $SecurePassword)
 
 
-function Get-AutomationAsset {
+function Get-AutomationAsset 
+{
     param(
-        [Parameter(Mandatory=$True)]
+        [Parameter(Mandatory = $True)]
         [ValidateSet('Variable', 'Certificate', 'PSCredential', 'Connection')]
         [string] $Type,
 
-        [Parameter(Mandatory=$True)]
+        [Parameter(Mandatory = $True)]
         [string]$Name
     )
 
     $MaxSecondsToWaitOnJobCompletion = 600
     $SleepTime = 5
-    $DoneJobStatuses = @("Completed", "Failed", "Stopped", "Blocked", "Suspended")
+    $DoneJobStatuses = @('Completed', 'Failed', 'Stopped', 'Blocked', 'Suspended')
 
     # Call Get-AutomationAsset runbook in SMA to get the asset value in serialized form
     $Params = @{
-        "Type" = $Type;
-        "Name" = $Name
+        'Type' = $Type
+        'Name' = $Name
     }
 
-    $Job = Start-SmaRunbook -Name "Get-AutomationAsset" -Parameters $Params @SmaWebServiceDetails
+    $Job = Start-SmaRunbook -Name 'Get-AutomationAsset' -Parameters $Params @SmaWebServiceDetails
 
-    if(!$Job) {
-        Write-Error "Unable to start the 'Get-AutomationAsset' runbook. Make sure it exists and is published in SMA."
+    if(!$Job) 
+    {
+        Write-Error -Message "Unable to start the 'Get-AutomationAsset' runbook. Make sure it exists and is published in SMA."
     }
-    else {
+    else 
+    {
         # Wait for Get-AutomationAsset completion
         $TotalSeconds = 0
         $JobInfo = $null
 
-        do {
+        do 
+        {
             Start-Sleep -Seconds $SleepTime
             $TotalSeconds += $SleepTime
 
             $JobInfo = Get-SmaJob -Id $Job @SmaWebServiceDetails
-        } while((!$DoneJobStatuses.Contains($JobInfo.JobStatus)) -and ($TotalSeconds -lt $MaxSecondsToWaitOnJobCompletion))
-
-        if($TotalSeconds -ge $MaxSecondsToWaitOnJobCompletion) {
-            Write-Error "Timeout exceeded. 'Get-AutomationAsset' job $Job did not complete in $MaxSecondsToWaitOnJobCompletion seconds."
         }
-        elseif($JobInfo.JobException) {
+        while((!$DoneJobStatuses.Contains($JobInfo.JobStatus)) -and ($TotalSeconds -lt $MaxSecondsToWaitOnJobCompletion))
+
+        if($TotalSeconds -ge $MaxSecondsToWaitOnJobCompletion) 
+        {
+            Write-Error -Message "Timeout exceeded. 'Get-AutomationAsset' job $Job did not complete in $MaxSecondsToWaitOnJobCompletion seconds."
+        }
+        elseif($JobInfo.JobException) 
+        {
             Write-Error ("'Get-AutomationAsset' job $Job threw exception: `n" + $JobInfo.JobException)
         }
-        else {
+        else 
+        {
             $SerializedOutput = Get-SmaJobOutput -Id $Job -Stream Output @SmaWebServiceDetails
             
             $Output = [System.Management.Automation.PSSerializer]::Deserialize($SerializedOutput.StreamText)  
@@ -71,47 +79,53 @@ function Get-AutomationAsset {
     }
 }
 
-function Get-AutomationConnection {
+function Get-AutomationConnection 
+{
     param(
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory = $True)]
         [string] $Name
     )
 
     Get-AutomationAsset -Type Connection -Name $Name
 }
 
-function Set-AutomationVariable {
+function Set-AutomationVariable 
+{
     param(
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory = $True)]
         [string] $Name,
 
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory = $True)]
         [object] $Value
     )
 
     $Variable = Get-SmaVariable -Name $Name @SmaWebServiceDetails
 
-     if($Variable) {
-        if($Variable.IsEncrypted) {
-            $output = Set-SmaVariable -Name $Name -Value $Value -Encrypted @SmaWebServiceDetails
+    if($Variable) 
+    {
+        if($Variable.IsEncrypted) 
+        {
+            $Output = Set-SmaVariable -Name $Name -Value $Value -Encrypted @SmaWebServiceDetails
         }
-        else {
-            $output = Set-SmaVariable -Name $Name -Value $Value -Force @SmaWebServiceDetails
+        else 
+        {
+            $Output = Set-SmaVariable -Name $Name -Value $Value -Force @SmaWebServiceDetails
         }
-     }
-
+    }
 }
 
-function Get-AutomationCertificate {
+function Get-AutomationCertificate 
+{
     param(
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory = $True)]
         [string] $Name
     )
 
     $Thumbprint = Get-AutomationAsset -Type Certificate -Name $Name
     
-    if($Thumbprint) {
-        $Cert = Get-Item "Cert:\CurrentUser\My\$Thumbprint"
+    if($Thumbprint) 
+    {
+        $Cert = Get-Item -Path "Cert:\CurrentUser\My\$Thumbprint"
 
         $Cert
     }
