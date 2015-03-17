e0fd7a7d8ecd71fee0d2ac70bac114259a3b8c13
Ryan Andorfer <Ryan.Andorfer@gmail.com>
2015-03-17 16:33:46 -0500

    update invoke-git to handle module import correctly and refactor

--- Runbooks/SMAContinuousIntegration/Invoke-GitRepositorySync.ps1
+++ Runbooks/SMAContinuousIntegration/Invoke-GitRepositorySync.ps1
@@ -69,15 +69,37 @@ Workflow Invoke-GitRepositorySync
                                          -RepositoryName $RepositoryName
                 Checkpoint-Workflow
             }
-            foreach($Module in $ReturnInformation.ModuleFiles)
+            foreach($ModulePath in $ReturnInformation.ModuleFiles)
             {
-                $ModuleName = (Test-ModuleManifest -Path $Module).Name
-                $PowerShellModuleInformation = Import-SmaPowerShellModule -ModuleName $ModuleName `
-                                                                          -WebserviceEndpoint $CIVariables.WebserviceEndpoint `
-                                                                          -WebservicePort $CIVariables.WebservicePort `
-                                                                          -Credential $SMACred
-                SetAutomationModuleActivityMetadata -ModuleName $PowerShellModuleInformation.ModuleName `
-                                                    -ModuleVersion $PowerShellModuleInformation.Version
+                Try
+                {
+                    $PowerShellModuleInformation = Test-ModuleManifest -Path $ModulePath
+                    $ModuleName = $PowerShellModuleInformation.Name -as [string]
+                    $ModuleVersion = $PowerShellModuleInformation.Version -as [string]
+                    $PowerShellModuleInformation = Import-SmaPowerShellModule -ModulePath $ModulePath `
+                                                                              -WebserviceEndpoint $CIVariables.WebserviceEndpoint `
+                                                                              -WebservicePort $CIVariables.WebservicePort `
+                                                                              -Credential $SMACred
+                    SetAutomationModuleActivityMetadata -ModuleName $ModuleName `
+                                                        -ModuleVersion $ModuleVersion
+                }
+                Catch
+                {
+                    $Exception = New-Exception -Type 'ImportSmaPowerShellModuleFailure' `
+                                               -Message 'Failed to import a PowerShell module into Sma' `
+                                               -Property @{
+                        'ErrorMessage' = (Convert-ExceptionToString $_) ;
+                        'ModulePath' = $ModulePath ;
+                        'ModuleName' = $ModuleName ;
+                        'ModuleVersion' = $ModuleVersion ;
+                        'PowerShellModuleInformation' = "$(ConvertTo-JSON $PowerShellModuleInformation)" ;
+                        'WebserviceEnpoint' = $CIVariables.WebserviceEndpoint ;
+                        'Port' = $CIVariables.WebservicePort ;
+                        'Credential' = $SMACred.UserName ;
+                    }
+                    Write-Exception -Exception $Exception -Stream Warning
+                }
+                
                 Checkpoint-Workflow
             }
             
@@ -98,11 +120,28 @@ Workflow Invoke-GitRepositorySync
             }
             if($ReturnInformation.ModuleFiles)
             {
-                $RepositoryModulePath = "$($RepositoryInformation.Path)\$($RepositoryInformation.PowerShellModuleFolder)"
-                inlinescript
+                Try
                 {
-                    Add-PSEnvironmentPathLocation -Path $Using:RepositoryModulePath
-                } -PSComputerName $RunbookWorker -PSCredential $SMACred
+                    Write-Verbose -Message 'Validating Module Path on Runbook Wokers'
+                    $RepositoryModulePath = "$($RepositoryInformation.Path)\$($RepositoryInformation.PowerShellModuleFolder)"
+                    inlinescript
+                    {
+                        Add-PSEnvironmentPathLocation -Path $Using:RepositoryModulePath
+                    } -PSComputerName $RunbookWorker -PSCredential $SMACred
+                    Write-Verbose -Message 'Finished Validating Module Path on Runbook Wokers'
+                }
+                Catch
+                {
+                    $Exception = New-Exception -Type 'PowerShellModulePathValidationError' `
+                                               -Message 'Failed to set PSModulePath' `
+                                               -Property @{
+                        'ErrorMessage' = (Convert-ExceptionToString $_) ;
+                        'RepositoryModulePath' = $RepositoryModulePath ;
+                        'RunbookWorker' = $RunbookWorker ;
+                    }
+                    Write-Exception -Exception $Exception -Stream Warning
+                }
+                
                 Checkpoint-Workflow
             }
             $UpdatedRepositoryInformation = (Set-SmaRepositoryInformationCommitVersion -RepositoryInformation $CIVariables.RepositoryInformation `
@@ -125,10 +164,10 @@ Workflow Invoke-GitRepositorySync
 }
 
 # SIG # Begin signature block
-# MIIOfQYJKoZIhvcNAQcCoIIObjCCDmoCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# MIID1QYJKoZIhvcNAQcCoIIDxjCCA8ICAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
 # gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUnzeaZHhX4YQaoyQWImoEXtxM
-# jNmgggqQMIIB8zCCAVygAwIBAgIQEdV66iePd65C1wmJ28XdGTANBgkqhkiG9w0B
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQU6JM64s8E2vomcAtezNDjJPzx
+# bDGgggH3MIIB8zCCAVygAwIBAgIQEdV66iePd65C1wmJ28XdGTANBgkqhkiG9w0B
 # AQUFADAUMRIwEAYDVQQDDAlTQ09yY2hEZXYwHhcNMTUwMzA5MTQxOTIxWhcNMTkw
 # MzA5MDAwMDAwWjAUMRIwEAYDVQQDDAlTQ09yY2hEZXYwgZ8wDQYJKoZIhvcNAQEB
 # BQADgY0AMIGJAoGBANbZ1OGvnyPKFcCw7nDfRgAxgMXt4YPxpX/3rNVR9++v9rAi
@@ -138,69 +177,12 @@ Workflow Invoke-GitRepositorySync
 # ty2pMj8+MRAFTTAOBgNVHQ8BAf8EBAMCB4AwDQYJKoZIhvcNAQEFBQADgYEAoK7K
 # SmNLQ++VkzdvS8Vp5JcpUi0GsfEX2AGWZ/NTxnMpyYmwEkzxAveH1jVHgk7zqglS
 # OfwX2eiu0gvxz3mz9Vh55XuVJbODMfxYXuwjMjBV89jL0vE/YgbRAcU05HaWQu2z
-# nkvaq1yD5SJIRBooP7KkC/zCfCWRTnXKWVTw7hwwggPuMIIDV6ADAgECAhB+k+v7
-# fMZOWepLmnfUBvw7MA0GCSqGSIb3DQEBBQUAMIGLMQswCQYDVQQGEwJaQTEVMBMG
-# A1UECBMMV2VzdGVybiBDYXBlMRQwEgYDVQQHEwtEdXJiYW52aWxsZTEPMA0GA1UE
-# ChMGVGhhd3RlMR0wGwYDVQQLExRUaGF3dGUgQ2VydGlmaWNhdGlvbjEfMB0GA1UE
-# AxMWVGhhd3RlIFRpbWVzdGFtcGluZyBDQTAeFw0xMjEyMjEwMDAwMDBaFw0yMDEy
-# MzAyMzU5NTlaMF4xCzAJBgNVBAYTAlVTMR0wGwYDVQQKExRTeW1hbnRlYyBDb3Jw
-# b3JhdGlvbjEwMC4GA1UEAxMnU3ltYW50ZWMgVGltZSBTdGFtcGluZyBTZXJ2aWNl
-# cyBDQSAtIEcyMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsayzSVRL
-# lxwSCtgleZEiVypv3LgmxENza8K/LlBa+xTCdo5DASVDtKHiRfTot3vDdMwi17SU
-# AAL3Te2/tLdEJGvNX0U70UTOQxJzF4KLabQry5kerHIbJk1xH7Ex3ftRYQJTpqr1
-# SSwFeEWlL4nO55nn/oziVz89xpLcSvh7M+R5CvvwdYhBnP/FA1GZqtdsn5Nph2Up
-# g4XCYBTEyMk7FNrAgfAfDXTekiKryvf7dHwn5vdKG3+nw54trorqpuaqJxZ9YfeY
-# cRG84lChS+Vd+uUOpyyfqmUg09iW6Mh8pU5IRP8Z4kQHkgvXaISAXWp4ZEXNYEZ+
-# VMETfMV58cnBcQIDAQABo4H6MIH3MB0GA1UdDgQWBBRfmvVuXMzMdJrU3X3vP9vs
-# TIAu3TAyBggrBgEFBQcBAQQmMCQwIgYIKwYBBQUHMAGGFmh0dHA6Ly9vY3NwLnRo
-# YXd0ZS5jb20wEgYDVR0TAQH/BAgwBgEB/wIBADA/BgNVHR8EODA2MDSgMqAwhi5o
-# dHRwOi8vY3JsLnRoYXd0ZS5jb20vVGhhd3RlVGltZXN0YW1waW5nQ0EuY3JsMBMG
-# A1UdJQQMMAoGCCsGAQUFBwMIMA4GA1UdDwEB/wQEAwIBBjAoBgNVHREEITAfpB0w
-# GzEZMBcGA1UEAxMQVGltZVN0YW1wLTIwNDgtMTANBgkqhkiG9w0BAQUFAAOBgQAD
-# CZuPee9/WTCq72i1+uMJHbtPggZdN1+mUp8WjeockglEbvVt61h8MOj5aY0jcwsS
-# b0eprjkR+Cqxm7Aaw47rWZYArc4MTbLQMaYIXCp6/OJ6HVdMqGUY6XlAYiWWbsfH
-# N2qDIQiOQerd2Vc/HXdJhyoWBl6mOGoiEqNRGYN+tjCCBKMwggOLoAMCAQICEA7P
-# 9DjI/r81bgTYapgbGlAwDQYJKoZIhvcNAQEFBQAwXjELMAkGA1UEBhMCVVMxHTAb
-# BgNVBAoTFFN5bWFudGVjIENvcnBvcmF0aW9uMTAwLgYDVQQDEydTeW1hbnRlYyBU
-# aW1lIFN0YW1waW5nIFNlcnZpY2VzIENBIC0gRzIwHhcNMTIxMDE4MDAwMDAwWhcN
-# MjAxMjI5MjM1OTU5WjBiMQswCQYDVQQGEwJVUzEdMBsGA1UEChMUU3ltYW50ZWMg
-# Q29ycG9yYXRpb24xNDAyBgNVBAMTK1N5bWFudGVjIFRpbWUgU3RhbXBpbmcgU2Vy
-# dmljZXMgU2lnbmVyIC0gRzQwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIB
-# AQCiYws5RLi7I6dESbsO/6HwYQpTk7CY260sD0rFbv+GPFNVDxXOBD8r/amWltm+
-# YXkLW8lMhnbl4ENLIpXuwitDwZ/YaLSOQE/uhTi5EcUj8mRY8BUyb05Xoa6IpALX
-# Kh7NS+HdY9UXiTJbsF6ZWqidKFAOF+6W22E7RVEdzxJWC5JH/Kuu9mY9R6xwcueS
-# 51/NELnEg2SUGb0lgOHo0iKl0LoCeqF3k1tlw+4XdLxBhircCEyMkoyRLZ53RB9o
-# 1qh0d9sOWzKLVoszvdljyEmdOsXF6jML0vGjG/SLvtmzV4s73gSneiKyJK4ux3DF
-# vk6DJgj7C72pT5kI4RAocqrNAgMBAAGjggFXMIIBUzAMBgNVHRMBAf8EAjAAMBYG
-# A1UdJQEB/wQMMAoGCCsGAQUFBwMIMA4GA1UdDwEB/wQEAwIHgDBzBggrBgEFBQcB
-# AQRnMGUwKgYIKwYBBQUHMAGGHmh0dHA6Ly90cy1vY3NwLndzLnN5bWFudGVjLmNv
-# bTA3BggrBgEFBQcwAoYraHR0cDovL3RzLWFpYS53cy5zeW1hbnRlYy5jb20vdHNz
-# LWNhLWcyLmNlcjA8BgNVHR8ENTAzMDGgL6AthitodHRwOi8vdHMtY3JsLndzLnN5
-# bWFudGVjLmNvbS90c3MtY2EtZzIuY3JsMCgGA1UdEQQhMB+kHTAbMRkwFwYDVQQD
-# ExBUaW1lU3RhbXAtMjA0OC0yMB0GA1UdDgQWBBRGxmmjDkoUHtVM2lJjFz9eNrwN
-# 5jAfBgNVHSMEGDAWgBRfmvVuXMzMdJrU3X3vP9vsTIAu3TANBgkqhkiG9w0BAQUF
-# AAOCAQEAeDu0kSoATPCPYjA3eKOEJwdvGLLeJdyg1JQDqoZOJZ+aQAMc3c7jecsh
-# aAbatjK0bb/0LCZjM+RJZG0N5sNnDvcFpDVsfIkWxumy37Lp3SDGcQ/NlXTctlze
-# vTcfQ3jmeLXNKAQgo6rxS8SIKZEOgNER/N1cdm5PXg5FRkFuDbDqOJqxOtoJcRD8
-# HHm0gHusafT9nLYMFivxf1sJPZtb4hbKE4FtAC44DagpjyzhsvRaqQGvFZwsL0kb
-# 2yK7w/54lFHDhrGCiF3wPbRRoXkzKy57udwgCRNx62oZW8/opTBXLIlJP7nPf8m/
-# PiJoY1OavWl0rMUdPH+S4MO8HNgEdTGCA1cwggNTAgEBMCgwFDESMBAGA1UEAwwJ
-# U0NPcmNoRGV2AhAR1XrqJ493rkLXCYnbxd0ZMAkGBSsOAwIaBQCgeDAYBgorBgEE
-# AYI3AgEMMQowCKACgAChAoAAMBkGCSqGSIb3DQEJAzEMBgorBgEEAYI3AgEEMBwG
-# CisGAQQBgjcCAQsxDjAMBgorBgEEAYI3AgEVMCMGCSqGSIb3DQEJBDEWBBSUaCiA
-# m8/pbHaKxStUX17kx4I7yzANBgkqhkiG9w0BAQEFAASBgJajRYM0pQCQp/UlzXez
-# c0oT3zM0riCkNUTuhU3af8KOZLQIvZuAy45eryp3x6Tm9lTelBcEVpm/oURSdMBI
-# fOFSsABUz5P6IgrtZqW9CD75gwLzo1TadxRJBzzNO/t5I0+t2J6Mh8oJVybpxNnB
-# A2yzw94ZREk16sOSJ5OafmXpoYICCzCCAgcGCSqGSIb3DQEJBjGCAfgwggH0AgEB
-# MHIwXjELMAkGA1UEBhMCVVMxHTAbBgNVBAoTFFN5bWFudGVjIENvcnBvcmF0aW9u
-# MTAwLgYDVQQDEydTeW1hbnRlYyBUaW1lIFN0YW1waW5nIFNlcnZpY2VzIENBIC0g
-# RzICEA7P9DjI/r81bgTYapgbGlAwCQYFKw4DAhoFAKBdMBgGCSqGSIb3DQEJAzEL
-# BgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTE1MDMxNzIwNTQ0OFowIwYJKoZI
-# hvcNAQkEMRYEFD4lFTXq/g3tIfba/FGPDcbivC3UMA0GCSqGSIb3DQEBAQUABIIB
-# ABxlHdTRba0nNceB7tw+v1xZvkfL6kfTh2kUuEYlUQtC/eT+I+KJ7BOXjNn7JWUp
-# B5TKMQkTV/c4X2ghlOr9pnZEUs0jDIAmMPLDTrWQMijBBAw1hMazu4FQjLbXTHH/
-# Vu0nAC1hoDp3osHKocpl1/Dkvwbm+jKdVK7LwI0HYsh3IHQO5RtYp4UzuBRmYGrE
-# 5ArUdiBEJwAr6U1/mLelEJNBu2W+LB/S7mtYAtSF9gU0egWQHXxnwrCmmi0sVPfY
-# qICbSmvRdd15ZzEQ+u8xW+CNN6rxSghMRrE5MP6Z783+uJhJVfhhM5OJiQWQK+3N
-# QoXk3EJjH1hGc/qQ16griWk=
+# nkvaq1yD5SJIRBooP7KkC/zCfCWRTnXKWVTw7hwxggFIMIIBRAIBATAoMBQxEjAQ
+# BgNVBAMMCVNDT3JjaERldgIQEdV66iePd65C1wmJ28XdGTAJBgUrDgMCGgUAoHgw
+# GAYKKwYBBAGCNwIBDDEKMAigAoAAoQKAADAZBgkqhkiG9w0BCQMxDAYKKwYBBAGC
+# NwIBBDAcBgorBgEEAYI3AgELMQ4wDAYKKwYBBAGCNwIBFTAjBgkqhkiG9w0BCQQx
+# FgQUo5iuo4p0hgX2EMi/nwU/TCmT7bQwDQYJKoZIhvcNAQEBBQAEgYC0UmobFb28
+# bQseJBP9B6JLVt+M4tvm64Z1D8M6be27gYQiQ6AiJUf9tiuEAPjHtyC2foC3RSnr
+# 9vM5FhL5zlFT81sApWi+CowFMsybyGeTujiBLB8/BT9MOKdKtjJCfztjQAC0SUB7
+# BHCfI0j0Erg4l1xro86pz/qMU6p5m2W13w==
 # SIG # End signature block
