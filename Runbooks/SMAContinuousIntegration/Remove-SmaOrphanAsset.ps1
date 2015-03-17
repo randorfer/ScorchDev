84681876346ac9738f3739302db15c24daf7f91e
Ryan Andorfer <Ryan.Andorfer@gmail.com>
2015-03-17 16:08:26 -0500

    refactor and handle errors

--- Runbooks/SMAContinuousIntegration/Remove-SmaOrphanAsset.ps1
+++ Runbooks/SMAContinuousIntegration/Remove-SmaOrphanAsset.ps1
@@ -1,94 +1,145 @@
 ﻿<#
-    .Synopsis
-        Checks a SMA environment and removes any global assets tagged
-        with the current repository that are no longer found in
-        the repository
+.Synopsis
+    Checks a SMA environment and removes any global assets tagged
+    with the current repository that are no longer found in
+    the repository
 
-    .Parameter RepositoryName
-        The name of the repository
+.Parameter RepositoryName
+    The name of the repository
 #>
 Workflow Remove-SmaOrphanAsset
 {
-   Param($RepositoryName)
+    Param($RepositoryName)
 
     Write-Verbose -Message "Starting [$WorkflowCommandName]"
     $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
-
-    $CIVariables = Get-BatchAutomationVariable -Name @('RepositoryInformation',
-                                                       'SMACredName',
+    Try
+    {
+        $CIVariables = Get-BatchAutomationVariable -Name @('RepositoryInformation', 
+                                                       'SMACredName', 
                                                        'WebserviceEndpoint'
                                                        'WebservicePort') `
-                                               -Prefix 'SMAContinuousIntegration'
-    $SMACred = Get-AutomationPSCredential -Name $CIVariables.SMACredName
+                                                   -Prefix 'SMAContinuousIntegration'
+        $SMACred = Get-AutomationPSCredential -Name $CIVariables.SMACredName
 
-    $RepositoryInformation = (ConvertFrom-JSON -InputObject $CIVariables.RepositoryInformation)."$RepositoryName"
+        $RepositoryInformation = (ConvertFrom-Json -InputObject $CIVariables.RepositoryInformation)."$RepositoryName"
 
-    $SmaVariables = Get-SmaVariable -WebServiceEndpoint $CIVariables.WebserviceEndpoint `
-                                    -Port $CIVariables.WebservicePort `
-                                    -Credential $SMACred
-    if($SmaVariables) { $SmaVariableTable = Group-SmaAssetsByRepository -InputObject $SmaVariables }
-
-    $SmaSchedules = Get-SmaSchedule -WebServiceEndpoint $CIVariables.WebserviceEndpoint `
-                                    -Port $CIVariables.WebservicePort `
-                                    -Credential $SMACred
-    if($SmaSchedules) { $SmaScheduleTable = Group-SmaAssetsByRepository -InputObject $SmaSchedules }
-
-    $RepositoryAssets = Get-GitRepositoryAssetName -Path "$($RepositoryInformation.Path)\$($RepositoryInformation.RunbookFolder)"
-
-    if($SmaVariableTable."$RepositoryName")
-    {
-        $VariableDifferences = Compare-Object -ReferenceObject $SmaVariableTable."$RepositoryName".Name `
-                                              -DifferenceObject $RepositoryAssets.Variable
-        Foreach($Difference in $VariableDifferences)
+        $SmaVariables = Get-SmaVariable -WebServiceEndpoint $CIVariables.WebserviceEndpoint `
+                                        -Port $CIVariables.WebservicePort `
+                                        -Credential $SMACred
+        if($SmaVariables) 
         {
-            if($Difference.SideIndicator -eq '<=')
+            $SmaVariableTable = Group-SmaAssetsByRepository -InputObject $SmaVariables 
+        }
+
+        $SmaSchedules = Get-SmaSchedule -WebServiceEndpoint $CIVariables.WebserviceEndpoint `
+                                        -Port $CIVariables.WebservicePort `
+                                        -Credential $SMACred
+        if($SmaSchedules) 
+        {
+            $SmaScheduleTable = Group-SmaAssetsByRepository -InputObject $SmaSchedules 
+        }
+
+        $RepositoryAssets = Get-GitRepositoryAssetName -Path "$($RepositoryInformation.Path)\$($RepositoryInformation.RunbookFolder)"
+
+        if($SmaVariableTable."$RepositoryName")
+        {
+            $VariableDifferences = Compare-Object -ReferenceObject $SmaVariableTable."$RepositoryName".Name `
+                                                  -DifferenceObject $RepositoryAssets.Variable
+            Foreach($Difference in $VariableDifferences)
             {
-                Write-Verbose -Message "[$($Difference.InputObject)] Does not exist in Source Control"
-                Remove-SmaVariable -Name $Difference.InputObject `
-                                   -WebServiceEndpoint $CIVariables.WebserviceEndpoint `
-                                   -Port $CIVariables.WebservicePort `
-                                   -Credential $SMACred
-                Write-Verbose -Message "[$($Difference.InputObject)] Removed from SMA"
+                Try
+                {
+                    if($Difference.SideIndicator -eq '<=')
+                    {
+                        Write-Verbose -Message "[$($Difference.InputObject)] Does not exist in Source Control"
+                        Remove-SmaVariable -Name $Difference.InputObject `
+                                           -WebServiceEndpoint $CIVariables.WebserviceEndpoint `
+                                           -Port $CIVariables.WebservicePort `
+                                           -Credential $SMACred
+                        Write-Verbose -Message "[$($Difference.InputObject)] Removed from SMA"
+                    }
+                }
+                Catch
+                {
+                    $Exception = New-Exception -Type 'RemoveSmaAssetFailure' `
+                                                -Message 'Failed to remove a Sma Asset' `
+                                                -Property @{
+                        'ErrorMessage' = (Convert-ExceptionToString $_) ;
+                        'AssetName' = $Difference.InputObject ;
+                        'AssetType' = 'Variable' ;
+                        'WebserviceEnpoint' = $CIVariables.WebserviceEndpoint ;
+                        'Port' = $CIVariables.WebservicePort ;
+                        'Credential' = $SMACred.UserName ;
+                    }
+                    Write-Exception -Exception $Exception -Stream Warning
+                }
             }
         }
-    }
-    else
-    {
-        Write-Warning -Message "[$RepositoryName] No Variables found in environment for this repository" `
-                      -WarningAction Continue
-    }
-
-    if($SmaScheduleTable."$RepositoryName")
-    {
-        $ScheduleDifferences = Compare-Object -ReferenceObject $SmaScheduleTable."$RepositoryName".Name `
-                                              -DifferenceObject $RepositoryAssets.Schedule
-        Foreach($Difference in $ScheduleDifferences)
+        else
         {
-            if($Difference.SideIndicator -eq '<=')
+            Write-Warning -Message "[$RepositoryName] No Variables found in environment for this repository" `
+                          -WarningAction Continue
+        }
+
+        if($SmaScheduleTable."$RepositoryName")
+        {
+            $ScheduleDifferences = Compare-Object -ReferenceObject $SmaScheduleTable."$RepositoryName".Name `
+                                                  -DifferenceObject $RepositoryAssets.Schedule
+            Foreach($Difference in $ScheduleDifferences)
             {
-                Write-Verbose -Message "[$($Difference.InputObject)] Does not exist in Source Control"
-                Remove-SmaSchedule -Name $Difference.InputObject `
-                                   -WebServiceEndpoint $CIVariables.WebserviceEndpoint `
-                                   -Port $CIVariables.WebservicePort `
-                                   -Credential $SMACred
-                Write-Verbose -Message "[$($Difference.InputObject)] Removed from SMA"
+                Try
+                {
+                    if($Difference.SideIndicator -eq '<=')
+                    {
+                        Write-Verbose -Message "[$($Difference.InputObject)] Does not exist in Source Control"
+                        Remove-SmaSchedule -Name $Difference.InputObject `
+                                           -WebServiceEndpoint $CIVariables.WebserviceEndpoint `
+                                           -Port $CIVariables.WebservicePort `
+                                           -Credential $SMACred
+                        Write-Verbose -Message "[$($Difference.InputObject)] Removed from SMA"
+                    }
+                }
+                Catch
+                {
+                    $Exception = New-Exception -Type 'RemoveSmaAssetFailure' `
+                                                -Message 'Failed to remove a Sma Asset' `
+                                                -Property @{
+                        'ErrorMessage' = (Convert-ExceptionToString $_) ;
+                        'AssetName' = $Difference.InputObject ;
+                        'AssetType' = 'Schedule' ;
+                        'WebserviceEnpoint' = $CIVariables.WebserviceEndpoint ;
+                        'Port' = $CIVariables.WebservicePort ;
+                        'Credential' = $SMACred.UserName ;
+                    }
+                    Write-Exception -Exception $Exception -Stream Warning
+                }
             }
         }
+        else
+        {
+            Write-Warning -Message "[$RepositoryName] No Schedules found in environment for this repository" `
+                          -WarningAction Continue
+        }
     }
-    else
+    Catch
     {
-        Write-Warning -Message "[$RepositoryName] No Schedules found in environment for this repository" `
-                      -WarningAction Continue
+        $Exception = New-Exception -Type 'RemoveSmaOrphanAssetWorkflowFailure' `
+                                   -Message 'Unexpected error encountered in the Remove-SmaOrphanAsset workflow' `
+                                   -Property @{
+            'ErrorMessage' = (Convert-ExceptionToString $_) ;
+            'RepositoryName' = $RepositoryName ;
+        }
+        Write-Exception -Exception $Exception -Stream Warning
     }
-
     Write-Verbose -Message "Finished [$WorkflowCommandName]"
 }
 
 # SIG # Begin signature block
-# MIIOfQYJKoZIhvcNAQcCoIIObjCCDmoCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# MIID1QYJKoZIhvcNAQcCoIIDxjCCA8ICAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
 # gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUJeCYsC7eX7bQZBuaNLIQmpMo
-# WCOgggqQMIIB8zCCAVygAwIBAgIQEdV66iePd65C1wmJ28XdGTANBgkqhkiG9w0B
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUqv20xHKNI4Nx/n3q/Jnnzpmo
+# vUCgggH3MIIB8zCCAVygAwIBAgIQEdV66iePd65C1wmJ28XdGTANBgkqhkiG9w0B
 # AQUFADAUMRIwEAYDVQQDDAlTQ09yY2hEZXYwHhcNMTUwMzA5MTQxOTIxWhcNMTkw
 # MzA5MDAwMDAwWjAUMRIwEAYDVQQDDAlTQ09yY2hEZXYwgZ8wDQYJKoZIhvcNAQEB
 # BQADgY0AMIGJAoGBANbZ1OGvnyPKFcCw7nDfRgAxgMXt4YPxpX/3rNVR9++v9rAi
@@ -98,69 +149,12 @@ Workflow Remove-SmaOrphanAsset
 # ty2pMj8+MRAFTTAOBgNVHQ8BAf8EBAMCB4AwDQYJKoZIhvcNAQEFBQADgYEAoK7K
 # SmNLQ++VkzdvS8Vp5JcpUi0GsfEX2AGWZ/NTxnMpyYmwEkzxAveH1jVHgk7zqglS
 # OfwX2eiu0gvxz3mz9Vh55XuVJbODMfxYXuwjMjBV89jL0vE/YgbRAcU05HaWQu2z
-# nkvaq1yD5SJIRBooP7KkC/zCfCWRTnXKWVTw7hwwggPuMIIDV6ADAgECAhB+k+v7
-# fMZOWepLmnfUBvw7MA0GCSqGSIb3DQEBBQUAMIGLMQswCQYDVQQGEwJaQTEVMBMG
-# A1UECBMMV2VzdGVybiBDYXBlMRQwEgYDVQQHEwtEdXJiYW52aWxsZTEPMA0GA1UE
-# ChMGVGhhd3RlMR0wGwYDVQQLExRUaGF3dGUgQ2VydGlmaWNhdGlvbjEfMB0GA1UE
-# AxMWVGhhd3RlIFRpbWVzdGFtcGluZyBDQTAeFw0xMjEyMjEwMDAwMDBaFw0yMDEy
-# MzAyMzU5NTlaMF4xCzAJBgNVBAYTAlVTMR0wGwYDVQQKExRTeW1hbnRlYyBDb3Jw
-# b3JhdGlvbjEwMC4GA1UEAxMnU3ltYW50ZWMgVGltZSBTdGFtcGluZyBTZXJ2aWNl
-# cyBDQSAtIEcyMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsayzSVRL
-# lxwSCtgleZEiVypv3LgmxENza8K/LlBa+xTCdo5DASVDtKHiRfTot3vDdMwi17SU
-# AAL3Te2/tLdEJGvNX0U70UTOQxJzF4KLabQry5kerHIbJk1xH7Ex3ftRYQJTpqr1
-# SSwFeEWlL4nO55nn/oziVz89xpLcSvh7M+R5CvvwdYhBnP/FA1GZqtdsn5Nph2Up
-# g4XCYBTEyMk7FNrAgfAfDXTekiKryvf7dHwn5vdKG3+nw54trorqpuaqJxZ9YfeY
-# cRG84lChS+Vd+uUOpyyfqmUg09iW6Mh8pU5IRP8Z4kQHkgvXaISAXWp4ZEXNYEZ+
-# VMETfMV58cnBcQIDAQABo4H6MIH3MB0GA1UdDgQWBBRfmvVuXMzMdJrU3X3vP9vs
-# TIAu3TAyBggrBgEFBQcBAQQmMCQwIgYIKwYBBQUHMAGGFmh0dHA6Ly9vY3NwLnRo
-# YXd0ZS5jb20wEgYDVR0TAQH/BAgwBgEB/wIBADA/BgNVHR8EODA2MDSgMqAwhi5o
-# dHRwOi8vY3JsLnRoYXd0ZS5jb20vVGhhd3RlVGltZXN0YW1waW5nQ0EuY3JsMBMG
-# A1UdJQQMMAoGCCsGAQUFBwMIMA4GA1UdDwEB/wQEAwIBBjAoBgNVHREEITAfpB0w
-# GzEZMBcGA1UEAxMQVGltZVN0YW1wLTIwNDgtMTANBgkqhkiG9w0BAQUFAAOBgQAD
-# CZuPee9/WTCq72i1+uMJHbtPggZdN1+mUp8WjeockglEbvVt61h8MOj5aY0jcwsS
-# b0eprjkR+Cqxm7Aaw47rWZYArc4MTbLQMaYIXCp6/OJ6HVdMqGUY6XlAYiWWbsfH
-# N2qDIQiOQerd2Vc/HXdJhyoWBl6mOGoiEqNRGYN+tjCCBKMwggOLoAMCAQICEA7P
-# 9DjI/r81bgTYapgbGlAwDQYJKoZIhvcNAQEFBQAwXjELMAkGA1UEBhMCVVMxHTAb
-# BgNVBAoTFFN5bWFudGVjIENvcnBvcmF0aW9uMTAwLgYDVQQDEydTeW1hbnRlYyBU
-# aW1lIFN0YW1waW5nIFNlcnZpY2VzIENBIC0gRzIwHhcNMTIxMDE4MDAwMDAwWhcN
-# MjAxMjI5MjM1OTU5WjBiMQswCQYDVQQGEwJVUzEdMBsGA1UEChMUU3ltYW50ZWMg
-# Q29ycG9yYXRpb24xNDAyBgNVBAMTK1N5bWFudGVjIFRpbWUgU3RhbXBpbmcgU2Vy
-# dmljZXMgU2lnbmVyIC0gRzQwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIB
-# AQCiYws5RLi7I6dESbsO/6HwYQpTk7CY260sD0rFbv+GPFNVDxXOBD8r/amWltm+
-# YXkLW8lMhnbl4ENLIpXuwitDwZ/YaLSOQE/uhTi5EcUj8mRY8BUyb05Xoa6IpALX
-# Kh7NS+HdY9UXiTJbsF6ZWqidKFAOF+6W22E7RVEdzxJWC5JH/Kuu9mY9R6xwcueS
-# 51/NELnEg2SUGb0lgOHo0iKl0LoCeqF3k1tlw+4XdLxBhircCEyMkoyRLZ53RB9o
-# 1qh0d9sOWzKLVoszvdljyEmdOsXF6jML0vGjG/SLvtmzV4s73gSneiKyJK4ux3DF
-# vk6DJgj7C72pT5kI4RAocqrNAgMBAAGjggFXMIIBUzAMBgNVHRMBAf8EAjAAMBYG
-# A1UdJQEB/wQMMAoGCCsGAQUFBwMIMA4GA1UdDwEB/wQEAwIHgDBzBggrBgEFBQcB
-# AQRnMGUwKgYIKwYBBQUHMAGGHmh0dHA6Ly90cy1vY3NwLndzLnN5bWFudGVjLmNv
-# bTA3BggrBgEFBQcwAoYraHR0cDovL3RzLWFpYS53cy5zeW1hbnRlYy5jb20vdHNz
-# LWNhLWcyLmNlcjA8BgNVHR8ENTAzMDGgL6AthitodHRwOi8vdHMtY3JsLndzLnN5
-# bWFudGVjLmNvbS90c3MtY2EtZzIuY3JsMCgGA1UdEQQhMB+kHTAbMRkwFwYDVQQD
-# ExBUaW1lU3RhbXAtMjA0OC0yMB0GA1UdDgQWBBRGxmmjDkoUHtVM2lJjFz9eNrwN
-# 5jAfBgNVHSMEGDAWgBRfmvVuXMzMdJrU3X3vP9vsTIAu3TANBgkqhkiG9w0BAQUF
-# AAOCAQEAeDu0kSoATPCPYjA3eKOEJwdvGLLeJdyg1JQDqoZOJZ+aQAMc3c7jecsh
-# aAbatjK0bb/0LCZjM+RJZG0N5sNnDvcFpDVsfIkWxumy37Lp3SDGcQ/NlXTctlze
-# vTcfQ3jmeLXNKAQgo6rxS8SIKZEOgNER/N1cdm5PXg5FRkFuDbDqOJqxOtoJcRD8
-# HHm0gHusafT9nLYMFivxf1sJPZtb4hbKE4FtAC44DagpjyzhsvRaqQGvFZwsL0kb
-# 2yK7w/54lFHDhrGCiF3wPbRRoXkzKy57udwgCRNx62oZW8/opTBXLIlJP7nPf8m/
-# PiJoY1OavWl0rMUdPH+S4MO8HNgEdTGCA1cwggNTAgEBMCgwFDESMBAGA1UEAwwJ
-# U0NPcmNoRGV2AhAR1XrqJ493rkLXCYnbxd0ZMAkGBSsOAwIaBQCgeDAYBgorBgEE
-# AYI3AgEMMQowCKACgAChAoAAMBkGCSqGSIb3DQEJAzEMBgorBgEEAYI3AgEEMBwG
-# CisGAQQBgjcCAQsxDjAMBgorBgEEAYI3AgEVMCMGCSqGSIb3DQEJBDEWBBQfmO3a
-# KhJQmfm/ybyy7RfXoxYcKDANBgkqhkiG9w0BAQEFAASBgCChsh1sFKb7+0eCxCnc
-# ioJupH7AJucA4j9dQ64fqeHYynuGJh+rJGzgSj2Xu0Z2ey1oRRmQJ5oHafp2+iqm
-# VMjgSB8F/c/4OHkKvDvQXnW+crcPpX0dZTsM6Q0aU0z/07gv1T9a3M4FNtrCj60C
-# 9NuW0kq+yK42vhOGybBXsVXtoYICCzCCAgcGCSqGSIb3DQEJBjGCAfgwggH0AgEB
-# MHIwXjELMAkGA1UEBhMCVVMxHTAbBgNVBAoTFFN5bWFudGVjIENvcnBvcmF0aW9u
-# MTAwLgYDVQQDEydTeW1hbnRlYyBUaW1lIFN0YW1waW5nIFNlcnZpY2VzIENBIC0g
-# RzICEA7P9DjI/r81bgTYapgbGlAwCQYFKw4DAhoFAKBdMBgGCSqGSIb3DQEJAzEL
-# BgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTE1MDMxNjIwMTczMVowIwYJKoZI
-# hvcNAQkEMRYEFHSRayl64Io9ihq2343afhy2B1DnMA0GCSqGSIb3DQEBAQUABIIB
-# AGSEy4i0xVUAGpxniMXiQj2AWFAvPF6k43nvdbCegfbSW8WSXEV40sCOxXsksuht
-# 5I1u6ykkoazs14kyqT40u5QiIkLXcufkkPYYnHniourdCvdkAfdS1q8MopfGTdwF
-# mD4yzdtPOmW/heePmmEssN/rU1BcPJJVDCd9HfmQXKcJUM7GBwOIHQCL2wrcign5
-# tu1X+MWSpdd6lzBTEwLS8AqU9XVL5pdXNFrRI9X+ZlkgoV7lII6QAW/7tKrNNoHK
-# r01Iq7rkLyoKNmcifxyT++9Db10ruDths8hfHimUHhURSsdRCiCTxQnjELoQiTD8
-# IIvJBvkYYlu4rHRXQ0iF1GQ=
+# nkvaq1yD5SJIRBooP7KkC/zCfCWRTnXKWVTw7hwxggFIMIIBRAIBATAoMBQxEjAQ
+# BgNVBAMMCVNDT3JjaERldgIQEdV66iePd65C1wmJ28XdGTAJBgUrDgMCGgUAoHgw
+# GAYKKwYBBAGCNwIBDDEKMAigAoAAoQKAADAZBgkqhkiG9w0BCQMxDAYKKwYBBAGC
+# NwIBBDAcBgorBgEEAYI3AgELMQ4wDAYKKwYBBAGCNwIBFTAjBgkqhkiG9w0BCQQx
+# FgQU3OD9APY+r3nGaPdbrT4nj58LXYEwDQYJKoZIhvcNAQEBBQAEgYCDP5Qpw7Fk
+# PO6o+GdpbWmCqwbZ7GJhzX3lNzMubpyFNYCxwvY7Qx+MS/U+CC8CAlYrdIeSQBmT
+# u64352vjVOqcpANlH2RbCG68OAiAo5SSNWwqDJEOWDv8j/fJgQtW8srF/G4r10Os
+# x8sTaXuLb1vMv/z38nTq5Xbnr1mUDWN1iQ==
 # SIG # End signature block
