759ba6b992fc19095da3d0782f5aaa902349c987
Ryan Andorfer <ryan.andorfer@gmail.com>
2015-05-01 15:11:03 -0500

    default to UTF8 files

--- LocalPowerShellModules/LocalDev/LocalDev.psm1
+++ LocalPowerShellModules/LocalDev/LocalDev.psm1
@@ -286,7 +286,7 @@ Function Set-LocalDevAutomationVariable
                                                 'isEncrypted' = $isEncrypted })
     }
     
-    Set-Content -Path $SettingsFilePath -Value (ConvertTo-JSON $SettingsVars)
+    Set-Content -Path $SettingsFilePath -Value (ConvertTo-JSON $SettingsVars) -Encoding UTF8
     Read-SmaJSONVariables $SettingsFilePath
 }
 
@@ -350,7 +350,7 @@ Function Remove-LocalDevAutomationVariable
     if($SettingsVars.Variables.ContainsKey($Name))
     {
         $SettingsVars.Variables.Remove($Name)
-        Set-Content -Path $SettingsFilePath -Value (ConvertTo-JSON $SettingsVars)
+        Set-Content -Path $SettingsFilePath -Value (ConvertTo-JSON $SettingsVars) -Encoding UTF8
         Read-SmaJSONVariables $SettingsFilePath
     }
     else
@@ -404,55 +404,6 @@ Workflow Get-AutomationPSCredential
     $SecurePassword = $Val.Password | ConvertTo-SecureString -asPlainText -Force
     $Credential = New-Object -TypeName System.Management.Automation.PSCredential($Val.Username, $SecurePassword)
     $Credential
-}
-
-<#
-.SYNOPSIS
-    Gets one or credentials using Get-AutomationPSCredential.
-
-.DESCRIPTION
-    Get-BatchAutomationPSCredential takes a hashtable which maps a friendly name to
-    a credential name. Each credential in the hashtable will be retrieved using
-    Get-AutomationPSCredential, will be accessible by its friendly name via the
-    returned object.
-
-.PARAMETER Alias
-    A hashtable mapping credential friendly names to a name passed to Get-AutomationPSCredential.
-
-.EXAMPLE
-    PS > $Creds = Get-BatchAutomationPSCredential -Alias @{'TestCred' = 'GENMILLS\M3IS052'; 'TestCred2' = 'GENMILLS\M2IS254'}
-
-    PS > $Creds.TestCred
-
-
-    PSComputerName        : localhost
-    PSSourceJobInstanceId : e2d9e9dc-2740-49ef-87d6-34e3334324e4
-    UserName              : GENMILLS\M3IS052
-    Password              : System.Security.SecureString
-
-    PS > $Creds.TestCred2
-
-
-    PSComputerName        : localhost
-    PSSourceJobInstanceId : 383da6c1-03f7-4b74-afc6-30e901972a5e
-    UserName              : GENMILLS.com\M2IS254
-    Password              : System.Security.SecureString
-#>
-
-Workflow Get-BatchAutomationPSCredential
-{
-    param(
-        [Parameter(Mandatory=$True)] [Hashtable] $Alias
-    )
-
-    $Creds = New-Object -TypeName 'PSObject'
-    foreach($Key in $Alias.Keys)
-    {
-        $Cred = Get-AutomationPSCredential -Name $Alias[$Key]
-        Add-Member -InputObject $Creds -Name $Key -Value $Cred -MemberType NoteProperty -Force
-        Write-Verbose -Message "Credential [$($Key)] = [$($Alias[$Key])]"
-    }
-    return $Creds
 }
 
 Function Set-LocalDevAutomationSchedule
@@ -571,7 +522,7 @@ Function Set-LocalDevAutomationSchedule
         }
     }
     
-    Set-Content -Path $SettingsFilePath -Value (ConvertTo-JSON $SettingsVars)
+    Set-Content -Path $SettingsFilePath -Value (ConvertTo-JSON $SettingsVars) -Encoding UTF8
 }
 Function Remove-LocalDevAutomationSchedule
 {
@@ -624,7 +575,7 @@ Function Remove-LocalDevAutomationSchedule
     if(($SettingsVars.Schedules | Get-Member -MemberType NoteProperty).Name -Contains $Name)
     {
         $SettingsVars.Schedules = $SettingsVars.Schedules | Select-Object -Property * -ExcludeProperty $Name
-        Set-Content -Path $SettingsFilePath -Value (ConvertTo-JSON $SettingsVars)
+        Set-Content -Path $SettingsFilePath -Value (ConvertTo-JSON $SettingsVars) -Encoding UTF8
     }
     else
     {
