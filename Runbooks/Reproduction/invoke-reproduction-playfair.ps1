b96fa81319f37198a5e1f20896b303ef08b27154
Ryan Andorfer <ryan.andorfer@gmail.com>
2015-10-20 12:20:28 -0500

    update playfair workflow

--- Runbooks/Reproduction/invoke-reproduction-playfair.ps1
+++ Runbooks/Reproduction/invoke-reproduction-playfair.ps1
@@ -6,5 +6,6 @@ workflow invoke-reproduction-playfair
         $ElapsedTime = ((Get-Date)-$StartTime).TotalSeconds
         Write-Verbose -Message "Elapsed Time [$("{0:N0}" -f $ElapsedTime)] Seconds"
         Start-Sleep -Seconds 5
+        Checkpoint-Workflow
     }
 }
