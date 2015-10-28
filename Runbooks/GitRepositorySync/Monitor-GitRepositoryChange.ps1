ad92bc90d83c7a823eabef7f79c02c2ed7210de5
Ryan Andorfer <ryan.andorfer@gmail.com>
2015-10-28 08:04:42 -0500

    change to a psscript

--- Runbooks/GitRepositorySync/Monitor-GitRepositoryChange.ps1
+++ Runbooks/GitRepositorySync/Monitor-GitRepositoryChange.ps1
@@ -3,52 +3,48 @@
 .Synopsis
     Check GIT repository for new commits. If found sync the changes into
     the current SMA environment
-
-.Parameter RepositoryName
 #>
-Workflow Monitor-GitRepositoryChange
-{
-    Param(
-    )
-    $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+Param(
+)
+
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
     
-    $GlobalVars = Get-BatchAutomationVariable -Prefix 'Global' `
-                                              -Name 'AutomationAccountName',
-                                                    'SubscriptionName',
-                                                    'SubscriptionAccessCredentialName',
-                                                    'RunbookWorkerAccessCredentialName',
-                                                    'ResourceGroupName'
+$GlobalVars = Get-BatchAutomationVariable -Prefix 'Global' `
+                                            -Name 'AutomationAccountName',
+                                                'SubscriptionName',
+                                                'SubscriptionAccessCredentialName',
+                                                'RunbookWorkerAccessCredentialName',
+                                                'ResourceGroupName'
+do
+{
+    $NextRun = (Get-Date).AddSeconds(30)
+        
+    $RepositoryInformationJSON = Get-AutomationVariable -Name 'ContinuousIntegration-RepositoryInformation'
+    $SubscriptionAccessCredential = Get-AutomationPSCredential -Name $GlobalVars.SubscriptionAccessCredentialName
+    $RunbookWorkerAccessCredential = Get-AutomationPSCredential -Name $GlobalVars.RunbookWorkerAccessCredentialName
+        
+    Try
+    {
+        $UpdatedRepositoryInformation = Sync-GitRepositoryToAzureAutomation -AutomationAccountName $GlobalVars.AutomationAccountName `
+                                                                            -SubscriptionName $GlobalVars.SubscriptionName `
+                                                                            -SubscriptionAccessCredential $SubscriptionAccessCredential `
+                                                                            -RunbookWorkerAccessCredenial $RunbookWorkerAccessCredential `
+                                                                            -RepositoryInformationJSON $RepositoryInformationJSON `
+                                                                            -ResourceGroupName $GlobalVars.ResourceGroupName
+
+        Set-AutomationVariable -Name 'ContinuousIntegration-RepositoryInformation' `
+                                -Value $UpdatedRepositoryInformation
+    }
+    Catch
+    {
+        Write-Exception -Stream Warning -Exception $_
+    }
+
     do
     {
-        $NextRun = (Get-Date).AddSeconds(30)
-        
-        $RepositoryInformationJSON = Get-AutomationVariable -Name 'ContinuousIntegration-RepositoryInformation'
-        $SubscriptionAccessCredential = Get-AutomationPSCredential -Name $GlobalVars.SubscriptionAccessCredentialName
-        $RunbookWorkerAccessCredential = Get-AutomationPSCredential -Name $GlobalVars.RunbookWorkerAccessCredentialName
-        
-        Try
-        {
-            $UpdatedRepositoryInformation = Sync-GitRepositoryToAzureAutomation -AutomationAccountName $GlobalVars.AutomationAccountName `
-                                                                                -SubscriptionName $GlobalVars.SubscriptionName `
-                                                                                -SubscriptionAccessCredential $SubscriptionAccessCredential `
-                                                                                -RunbookWorkerAccessCredenial $RunbookWorkerAccessCredential `
-                                                                                -RepositoryInformationJSON $RepositoryInformationJSON `
-                                                                                -ResourceGroupName $GlobalVars.ResourceGroupName
-
-            Set-AutomationVariable -Name 'ContinuousIntegration-RepositoryInformation' `
-                                    -Value $UpdatedRepositoryInformation
-        }
-        Catch
-        {
-            Write-Exception -Stream Warning -Exception $_
-        }
-
-        do
-        {
-            Start-Sleep -Seconds 5
-            Checkpoint-Workflow
-            $Sleeping = (Get-Date) -lt $NextRun
-        } while($Sleeping)
-    }
-    while($true)
+        Start-Sleep -Seconds 5
+        Checkpoint-Workflow
+        $Sleeping = (Get-Date) -lt $NextRun
+    } while($Sleeping)
 }
+while($true)
