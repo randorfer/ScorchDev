6f8ede5bbe85199859b7791f03a6ccff6530276d
Ryan Andorfer <ryan.andorfer@gmail.com>
2015-10-28 08:47:26 -0500

    create runbook that can be invoked via web hook

---
+++ Runbooks/GitRepositorySync/Invoke-GitRepositorySync.ps1
@@ -0,0 +1,41 @@
+#requires -Version 3
+<#
+.Synopsis
+    Check GIT repository for new commits. If found sync the changes into
+    the current SMA environment
+#>
+Param(
+)
+
+$CompletedParams = Write-StartingMessage
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+    
+$GlobalVars = Get-BatchAutomationVariable -Prefix 'Global' `
+                                            -Name 'AutomationAccountName',
+                                                'SubscriptionName',
+                                                'SubscriptionAccessCredentialName',
+                                                'RunbookWorkerAccessCredentialName',
+                                                'ResourceGroupName'
+        
+$RepositoryInformationJSON = Get-AutomationVariable -Name 'ContinuousIntegration-RepositoryInformation'
+$SubscriptionAccessCredential = Get-AutomationPSCredential -Name $GlobalVars.SubscriptionAccessCredentialName
+$RunbookWorkerAccessCredential = Get-AutomationPSCredential -Name $GlobalVars.RunbookWorkerAccessCredentialName
+        
+Try
+{
+    $UpdatedRepositoryInformation = Sync-GitRepositoryToAzureAutomation -AutomationAccountName $GlobalVars.AutomationAccountName `
+                                                                        -SubscriptionName $GlobalVars.SubscriptionName `
+                                                                        -SubscriptionAccessCredential $SubscriptionAccessCredential `
+                                                                        -RunbookWorkerAccessCredenial $RunbookWorkerAccessCredential `
+                                                                        -RepositoryInformationJSON $RepositoryInformationJSON `
+                                                                        -ResourceGroupName $GlobalVars.ResourceGroupName
+
+    Set-AutomationVariable -Name 'ContinuousIntegration-RepositoryInformation' `
+                            -Value $UpdatedRepositoryInformation
+}
+Catch
+{
+    Write-Exception -Stream Warning -Exception $_
+}
+
+Write-CompletedMessage @CompletedParams
