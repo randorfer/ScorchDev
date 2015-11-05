57333ed78e1331eff9661cb0775963c3b6e39e12
Ryan Andorfer <Ryan.Andorfer@gmail.com>
2015-11-05 16:55:09 -0600

    test github integration test

--- Runbooks/GitRepositorySync/Invoke-GitRepositoryIntegrationTest.ps1
+++ Runbooks/GitRepositorySync/Invoke-GitRepositoryIntegrationTest.ps1
@@ -3,18 +3,42 @@
        Invokes an integration test
 #>
 Param(
-    $Path,
-    $Recipient
+    [object]
+    $webhookData
 )
+
 $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
 $CompletedParameters = Write-StartingMessage -CommandName 'Invoke-GitRepositoryIntegrationTest'
 
-$Vars = Get-BatchAutomationVariable -Name  'From', 'SmtpServer' `
+$Vars = Get-BatchAutomationVariable -Name  'From', 'SmtpServer', 'RepositoryInformation' `
                                     -Prefix 'ContinuousIntegration'
 
 Try
 {
-    $Results = Invoke-IntegrationTest -Path $Path
+    $RepositoryInformation = $Vars.RepositoryInformation | ConvertFrom-Json | ConvertFrom-PSCustomObject
+    $GithubData = $WebhookData.RequestBody | ConvertFrom-Json
+    Foreach($RepositoryName in $RepositoryInformation.Keys)
+    {
+        $_RepositoryInformation = $RepositoryInformation.$RepositoryName
+        if($_RepositoryInformation.RepositoryPath -like "$($GithubData.repository.url)*")
+        {
+            break
+        }
+        else
+        {
+            $_RepositoryInformation = $Null
+        }
+    }
+    if(-not $_RepositoryInformation -as [bool]) 
+    { 
+        Throw-Exception -Type 'RepositoryNotFound' -Message 'Repository Information not found in Repository Information'
+    }
+    $BeforeCommit = $GithubData.before
+    $AfterCommit = $GithubData.after
+    $Recipient = $GithubData.Pusher.Email
+    $PathsToCheck = $GithubData.head_commit.added | ForEach-Object { "$($_RepositoryInformation.Path)\$($_.Replace('/','\'))" }
+    $PathsToCheck += $GithubData.head_commit.modified | ForEach-Object { "$($_RepositoryInformation.Path)\$($_.Replace('/','\'))" }
+    $Results = $PathsToCheck | ForEach-Object { Invoke-IntegrationTest -Path $_ }
     $TempDirectory = New-TempDirectory
     $ResultFile = "$TempDirectory\Results.txt"
     $Results | ConvertTo-Json -Depth ([int]::MaxValue) > $ResultFile
@@ -22,7 +46,9 @@ Try
                      -Attachments $ResultFile `
                      -SmtpServer $Vars.SmtpServer `
                      -Subject 'Integration Test Result' `
-                     -From $Vars.From
+                     -From $Vars.From `
+                     -BodyAsHtml  `
+                     -Body "<a href='$($GithubData.compare)'>See Change</a><br />$($GithubData.head_commit | ConvertTo-Json)"
 }
 Catch
 {
