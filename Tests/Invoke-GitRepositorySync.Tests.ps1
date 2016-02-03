585a5e03c68c72403720f1ab2401c9671e5fa786
Local User <Local User>
2016-02-03 15:08:28 -0800

    Update to make more valid tests

--- Tests/Invoke-GitRepositorySync.Tests.ps1
+++ Tests/Invoke-GitRepositorySync.Tests.ps1
@@ -1,7 +1,27 @@
-﻿Select-LocalDevWorkspace SCOrchDev
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+Select-LocalDevWorkspace SCOrchDev
 
 Describe "Invoke-GitRepositorySync" {
-    It "does something useful" {
-        $true | Should Be $true
+    Mock Get-AutomationPSCredential {
+        return new-object -TypeName System.Management.Automation.PSCredential -ArgumentList 'user\a', ('asdf'|ConvertTo-SecureString -AsPlainText -Force)
+    }
+    Mock Connect-AzureRmAccount { 
+    }
+    
+    Mock Set-AutomationVariable {
+        Return $Value
+    }
+    Mock Sync-GitRepositoryToAzureAutomation {
+        '123'
+    }
+
+    $Output = & "$Here\../Runbooks/GitRepositorySync/Invoke-GitRepositorySync.ps1"
+    It 'Should establish a connection to azure' {
+        Assert-MockCalled -CommandName Connect-AzureRmAccount -Times 1
+    }
+
+    It 'Should call Sync Git Repository' {
+        Assert-MockCalled -CommandName Sync-GitRepositoryToAzureAutomation -Times 1
     }
 }
